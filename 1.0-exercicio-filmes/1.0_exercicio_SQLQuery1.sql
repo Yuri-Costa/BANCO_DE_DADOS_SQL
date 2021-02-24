ff391791d5187ced50ae7eb4ff78797f402ebb5b
yuri costa <yuri342costa@gmail.com>
2021-02-24 17:19:22 -0300

    modelo entidade feito

---
+++ 1.0-exercicio-filmes/1.0_exercicio_SQLQuery1.sql
@@ -0,0 +1,16 @@
+CREATE DATABASE filmes;
+
+USE filmes;
+
+CREATE TABLE Generos
+(
+	idGenero  INT PRIMARY KEY IDENTITY
+,   Nome	  VARCHAR(200)NOT NULL
+);
+
+CREATE TABLE filmes
+(
+	idFilmes INT PRIMARY KEY IDENTITY
+,	idGenero INT FOREIGN KEY REFERENCES Generos(idGenero)
+,	Titulo   VARCHAR(250) NOT NULL
+);
